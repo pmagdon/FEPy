6ceb20d0ccf8b5d5671b6d7f8d3fe6f396c91557
Paul Magdon <pmagdon@gwdg.de>
2016-01-07 10:05:51 +0100

    Change MOCK class

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,12 +14,17 @@
 
 import sys
 import os
-import mock
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
 
 #MOCK_MODULES = ['numpy', 'osgeo','osgeo.gdal','gdalconst','rasterio','lxml','datetime']
 MOCK_MODULES = ['osgeo','osgeo.gdal','gdalconst','rasterio','lxml','datetime']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
