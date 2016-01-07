5c22640001605cc3f24a0d5f7584c0490e92b306
Paul Magdon <pmagdon@gwdg.de>
2016-01-07 09:39:05 +0100

    Changed back to standard docstrings without numpydocs to solve issue in RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,7 @@ sys.path.append(os.path.join(os.path.dirname(__name__), '..\\fepy'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'numpydoc'
+    'sphinx.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
